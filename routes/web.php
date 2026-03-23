70f0dba6cd0c20ff1b4c011a9354ec5ef510c8f8
unknown <anthony.cerdas.chacon@est.una.ac.cr>
2026-03-23 11:50:35 -0600

    web.php y sidebar.blade.php

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ Route::view('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
+
+    Route::view('correo3', 'correos.correo3')->name('correo3');
+    Route::view('correo4', 'correos.correo4')->name('correo4');
 });
 
 require __DIR__.'/settings.php';
